d3a16d3aa7210037174216a6b2b71126e7a60075
peterjacobson <peterjacobson.nz@gmail.com>
2015-04-27 14:21:22 +1200

    setup.rb working

--- setup.rb
+++ setup.rb
@@ -1,8 +1,16 @@
 require 'faker'
-require_relative 'app/db'
+require_relative './app/db'
 
 module RedditDB
-	def self.setup
+	def self.reset
+		drop
+		create_tables
+		seed
+		tester
+	end
+
+	def self.drop
+		puts "dropping..."
 		drop_tables = <<-SQL
 				DROP TABLE IF EXISTS users;
 				DROP TABLE IF EXISTS stories;
@@ -10,16 +18,19 @@ module RedditDB
 				DROP TABLE IF EXISTS replies;
 			SQL
 		$db.execute_batch(drop_tables)
+	end
 
+	def self.create_tables
+		puts "creating_tables..."
 		create_tables = <<-SQL
 			CREATE TABLE IF NOT EXISTS 	users(
-				id		INT PRIMARY KEY 	NOT NULL,
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
 				user_name  	VARCHAR(64)		NOT NULL,
-				email 	VARCHAR(64)			NOT NULL		
+				email 	VARCHAR(64)			NOT NULL
 				);
 
 			CREATE TABLE IF NOT EXISTS 	stories(
-				id		INT PRIMARY KEY 	NOT NULL,
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
 				title  	VARCHAR(128)			NOT NULL,
 				user_id	INT					NOT NULL,
 				score 	INT					NOT NULL,
@@ -28,7 +39,7 @@ module RedditDB
 				);
 
 			CREATE TABLE IF NOT EXISTS 	comments(
-				id		INT PRIMARY KEY 	NOT NULL,		
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
 				story_id	INT					NOT NULL,
 				user_id	INT					NOT NULL,
 				score 	INT					NOT NULL,
@@ -38,7 +49,7 @@ module RedditDB
 				);
 
 			CREATE TABLE IF NOT EXISTS 	replies(
-				id		INT PRIMARY KEY 	NOT NULL,		
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
 				story_id	INT,
 				reply_id	INT,
 				user_id	INT,
@@ -50,29 +61,36 @@ module RedditDB
 				);
 			SQL
 		$db.execute_batch(create_tables)
+		# $db.execute(".tables")
 	end
-	def self.seed
-		populate_users = "INSERT INTO users(id, user_name, email) 
-											VALUES (?, ?, ?);"
-		populate_stories = "INSERT INTO stories(id, title, user_id, score, contents) 
-											VALUES (?, ?, ?, ?, ?);"
-		populate_comments = "INSERT INTO comments(id, story_id, user_id, score, contents) 
-											VALUES (?, ?, ?, ?, ?);"
-		populate_replies = "INSERT INTO replies(id, story_id, reply_id, user_id, score, contents) 
-											VALUES (?, ?, ?, ?, ?, ?);"
 
-		(3..100).each do |count|
-			$db.execute(populate_users, [count, Faker::Name.first_name, Faker::Internet.free_email])
-			$db.execute(populate_stories, [count, Faker::Lorem.sentence(10), rand(1..100), 
+	def self.seed
+		puts "seeding..."
+		populate_users = "INSERT INTO users(user_name, email)
+											VALUES (?, ?);"
+		populate_stories = "INSERT INTO stories(title, user_id, score, contents)
+											VALUES (?, ?, ?, ?);"
+		populate_comments = "INSERT INTO comments(story_id, user_id, score, contents)
+											VALUES (?, ?, ?, ?);"
+		populate_replies = "INSERT INTO replies(story_id, reply_id, user_id, score, contents)
+											VALUES (?, ?, ?, ?, ?);"
+
+		(1..10).each do |count|
+			$db.execute(populate_users, [Faker::Name.first_name, Faker::Internet.free_email])
+			$db.execute(populate_stories, [Faker::Lorem.sentence(10), rand(1..100),
 																			rand(0..49), Faker::Lorem.paragraph(2)])
-			$db.execute(populate_comments, [count, rand(1..100), rand(1..100), rand(1..20), 
+			$db.execute(populate_comments, [rand(1..100), rand(1..100), rand(1..20),
 																				Faker::Lorem.paragraph(1)])
-			$db.execute(populate_replies, [count, (count%2 === 0) ? rand(1..100) : nil, 
-																			(count%2 === 0) ? nil : rand(1..100), 
+			$db.execute(populate_replies, [(count%2 === 0) ? rand(1..100) : nil,
+																			(count%2 === 0) ? nil : rand(1..100),
 																			rand(1..100), rand(1..12), Faker::Lorem.paragraph(1)])
 		end
 	end
+
+	def self.tester
+		puts "testing..."
+		puts "tables populated! ready to go" if $db.execute("SELECT user_name FROM users LIMIT 5").length == 5
+	end
 end
 
-RedditDB.setup
-RedditDB.seed
+RedditDB.reset
